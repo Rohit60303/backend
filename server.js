01811f227430191f87c555d21925589c62f7463c
Rohit60303 <rohitvarmab448@gmail.com>
2025-10-28 18:05:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 // Enhanced Socket.IO configuration
 const io = socketIo(server, {
   cors: {
-    origin: "https://cowrite-three.vercel.app",
+    origin: "https://cowrite-git-main-bhupathiraju-rohit-varmas-projects.vercel.app?_vercel_share=w4Pat52jNsO0ZbGS6AmyGVPbG3mmxBFA",
     methods: ["GET", "POST"]
   },
   pingInterval: 5000,
@@ -20,7 +20,7 @@ const io = socketIo(server, {
 });
 
 app.use(cors((
-  origin: "https://cowrite-three.vercel.app",
+  origin: "https://cowrite-git-main-bhupathiraju-rohit-varmas-projects.vercel.app?_vercel_share=w4Pat52jNsO0ZbGS6AmyGVPbG3mmxBFA",
   methods: ["GET", "POST", "PUT"],
   credentials: true
 ));
